63c62ffe61a97b93c77892cf384582039dd3d9c8
Osagie Erhabor <erhaborosagie@gmail.com>
2020-06-08 17:15:48 +0100

    initial commit

---
+++ src/test/java/com/osagie/sendmail/SendMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osagie.sendmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
